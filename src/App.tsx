a9e1d5774e869f0194c5ec1cb22023809602f01a
Vahe Sevachyan <vscode13@gmail.com>
2023-11-30 22:29:23 -0800

    added boiler plate for bootstrap, routes, folders etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { About } from "./Pages/About";
+import { Home } from "./Pages/Home";
+import { Store } from "./Pages/Store";
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
