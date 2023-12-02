a2a2845de1c85652e3276570cbfd74270d4e0951
Vahe Sevachyan <vscode13@gmail.com>
2023-12-02 15:32:32 -0800

    added shopping cart button and icon

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,11 @@
-import { Container, Nav, Navbar as NavBarbs } from "react-bootstrap";
+import { Button, Container, Nav, Navbar as NavBarbs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
+import shoppingCart from "../assets/shopping-cart.svg";
 export const NavBar = () => {
   return (
     <NavBarbs className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav>
+        <Nav className="me-auto">
           <Nav.Link to="/Home" as={NavLink}>
             Home
           </Nav.Link>
@@ -16,6 +16,13 @@ export const NavBar = () => {
             About
           </Nav.Link>
         </Nav>
+        <Button>
+          <img
+            src={shoppingCart}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem", color: "white" }}
+          />
+        </Button>
       </Container>
     </NavBarbs>
   );
