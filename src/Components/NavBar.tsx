06007f73eed7fc51018f77a8e594719983e4621d
Vahe Sevachyan <vscode13@gmail.com>
2023-12-01 23:28:05 -0800

    added nav links with  navigation between pages

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,9 +1,22 @@
-import { Container, Navbar as NavBarbs } from "react-bootstrap";
+import { Container, Nav, Navbar as NavBarbs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <NavBarbs className="bg-white shadow-sm mb-3">
-      <Container>Nav</Container>
+      <Container>
+        <Nav>
+          <Nav.Link to="/Home" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/Store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/About" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+      </Container>
     </NavBarbs>
   );
 };
