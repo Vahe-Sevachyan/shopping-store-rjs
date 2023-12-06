30085c52ced3826558ae7029009cd0307f8e9184
Vahe Sevachyan <vscode13@gmail.com>
2023-12-05 20:22:32 -0800

    added remove button

--- src/Components/StoreItem.tsx
+++ src/Components/StoreItem.tsx
@@ -9,7 +9,7 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+  const quantity = 1;
   return (
     <Card className="h-100">
       <Card.Img
@@ -24,7 +24,30 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
-          {quantity === 0 ? <Button>+ Add To Cart</Button> : null}
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content=center"
+                style={{ gap: ".5rem" }}
+              >
+                <div>
+                  <Button>-</Button>
+                  <span className="fs-3">{quantity}</span>
+                  in cart
+                  <Button>+</Button>
+                </div>
+                <Button variant="danger" size="sm">
+                  Remove
+                </Button>
+              </div>
+              Bye
+            </div>
+          )}
         </div>
       </Card.Body>
     </Card>
